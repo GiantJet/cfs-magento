af624387f5b10b7ff4e947c8141d7c522b3dd23d
johnadapt <john@adaptinnovation.com>
2017-07-19 10:55:39 -0400

    initial build and theme-ing

--- magento/dev/tools/grunt/configs/themes.js
+++ magento/dev/tools/grunt/configs/themes.js
@@ -50,5 +50,15 @@ module.exports = {
             'css/styles'
         ],
         dsl: 'less'
+    },
+    cfsbinds: {
+        area: 'frontend',
+        name: 'solve/cfsbinds',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
     }
 };
